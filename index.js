5d5d5e418cc2772521805e48aa398827f4c112f5
George Almonte <georgeaf1825@gmail.com>
2019-03-15 23:41:53 -0400

    Added function to display categories sum and minor tweaks to code

--- index.js
+++ index.js
@@ -1,56 +1,134 @@
-const leftover_div = document.querySelector('.leftover-div');
+const balance_div = document.querySelector('.balance-div');
 const main_div = document.querySelector('.main');
 const expenses_div = document.querySelector('.expensesDiv');
 const budget_div = document.querySelector('.budget_div');
+const cat_div = document.querySelector('.cat_div');
 
-let budget_value = 500;
+const food_cat = document.createElement('p');
+const enter_cat = document.createElement('p');
+const bills_cat = document.createElement('p');
+const clothing_cat = document.createElement('p');
 
-function budget_valueF() {
-    return budget_value.toFixed(2);
+let balance = 500;//starting budget/balance to be set at the start of the web app, also sets the budget
+
+
+
+function balance_func() { //keeps balance from having more than 2 digits after floating point
+    return balance.toFixed(2);
 }
 
 
 class budgetPlanner{
     constructor(){
-        this.expenses = [
-            new Expense('Sandwich', 5.99, 'Food'),
-            new Expense('Movie', 11.99, 'Entertainment'),
-            new Expense('Car monthly payment', 49.56, 'Bills')
+        this.expenses = [ //main array containing the expenses 
+            new Expense('Sandwich', 5.99, 'food'),
+            new Expense('Movie', 11.99, 'entertainment'),
+            new Expense('Car monthly payment', 49.56, 'bills'),
+            new Expense('Jeans', 45.49, 'clothing'),
+            new Expense('Netflix', 12.99, 'entertainment'),
+            new Expense('Electricity', 32.56, 'bills'),
+            new Expense('Bikini', 30.43, 'clothing'),
+            new Expense('Orange', 1.24, 'food'),
+            new Expense('Sushi', 133.24, 'food')
             
         ];
     }
+
+/*************************************** Display functions start *****************************************/
+
+
+    display_categories(){ //displays money spent in each category
+        food_cat.innerHTML = '';        
+        enter_cat.innerHTML = '';        
+        bills_cat.innerHTML = '';        
+        clothing_cat.innerHTML = '';        
+        
+        let food_spent = 0;
+        let enter_spent = 0;
+        let bills_spent = 0 ;
+        let clothing_spent = 0;
+
+
+        //fetches the prices from expenses and stores them in a variable for each category
+        const categories = this.expenses.map( item => item.category);
+        const prices = this.expenses.map( item => item.price);
+        let i = 0;
+
+        for ( const element of categories){
+            
+            if(element === 'food'){
+                food_spent += parseFloat(prices[i]);
+            }
+
+            if(element === 'entertainment'){
+                enter_spent += parseFloat(prices[i]);
+            }
+
+            if(element === 'bills'){
+                bills_spent += parseFloat(prices[i]);
+            }
+
+            if(element === 'clothing'){
+                clothing_spent += parseFloat(prices[i]);
+            }
+
+            i++;
+        }
+        
+
+        //appends and update the content of each category
+        food_cat.className = 'cat';
+        food_cat.innerHTML = `Food: $${food_spent.toFixed(2)}`;
+        cat_div.appendChild(food_cat);
+
+        enter_cat.className = 'cat';
+        enter_cat.innerHTML = `Entertainment: $${enter_spent.toFixed(2)}`;
+        cat_div.appendChild(enter_cat);
+
+        bills_cat.className = 'cat';
+        bills_cat.innerHTML = `Bills: $${bills_spent.toFixed(2)}`;
+        cat_div.appendChild(bills_cat);
+
+        clothing_cat.className = 'cat';
+        clothing_cat.innerHTML = `Clothing: $${clothing_spent.toFixed(2)}`;
+        cat_div.appendChild(clothing_cat);
+
+
+        console.log( food_spent);
+        console.log( enter_spent);
+        console.log( bills_spent);
+        console.log( clothing_spent);
+    }
     
-    display(){
-        leftover_div.innerHTML= '';
+    display(){ //displays expenses
+        balance_div.innerHTML= '';
         expenses_div.innerHTML = '';
         
 
-        const budget = document.createElement('h1');
-        budget.innerHTML = `$${budget_valueF()}`;
+        const balance = document.createElement('h1');
+        balance.innerHTML = `$${balance_func()}`;
 
-        leftover_div.appendChild(budget);
+        balance_div.appendChild(balance);
         
-        main_div.appendChild(leftover_div);
-
-        
+        main_div.appendChild(balance_div);
 
 
 
-        for(let i = 0; i < this.expenses.length; i++){
-
+        //for loop that goes through every iteratioin to show each element in the main array
+         for(let i = 0; i < this.expenses.length; i++){  
             const item = this.expenses[i];
 
             const expense = document.createElement('div');
             expense.className = ('expense');
         
 
-            expense.innerHTML = `<p class = 'inline'>${item.name}</p> 
-            <p class = 'inline'>$${item.price}</p> 
+            expense.innerHTML = `<p class = 'expense_element inline'>${item.name}</p> 
+            <p class = 'expense_element inline'>$${item.price}</p> 
             `;
 
             const deleteIcon = document.createElement('i');
             
-            deleteIcon.className = ('deleteIcon inline far fa-trash-alt');
+            deleteIcon.className = ('expense_element deleteIcon inline far fa-trash-alt');
 
             
             expenses_div.appendChild(expense);
@@ -64,25 +142,30 @@ class budgetPlanner{
             this.display();
                 });
         }
+
+        this.display_categories();
     }
 
-    displayBudget = () => {
+    displayBudget = () => { //displays fixed budget
         const budget_p = document.createElement('p');
         
-        budget_p.innerText = `$${budget_value}`;
+        budget_p.innerText = `$${balance}`;
         
         main_div.appendChild(budget_p);
         
         }
 
-    delete(index){
+    delete(index){  //removes object from array when called and adds back balance
         
         
-        budget_value += this.expenses[index].price;
+        balance += this.expenses[index].price;
         
         this.expenses.splice(index,1);
     }
 }
+
+/*************************************** Display functions end *****************************************/
+
 
 class Expense{
     constructor(name, price, category){
