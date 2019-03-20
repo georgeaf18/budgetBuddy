35201e9316e22ef916cbaedf1774fb32267a6e72
George Almonte <georgeaf1825@gmail.com>
2019-03-20 19:58:51 -0400

    updated css and js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const balance_div = document.querySelector('.balance-div');
+const budget_div = document.querySelector('.budget_div');
 const main_div = document.querySelector('.main');
 const expenses_div = document.querySelector('.expensesDiv');
-const budget_div = document.querySelector('.budget_div');
 const cat_div = document.querySelector('.cat_div');
 const rightIcon = document.querySelector('.right');
 const leftIcon = document.querySelector('.left');
+const week_paragraph = document.querySelector('.arrows p');
+const balanceHeader = document.createElement('p');
+
 
 const food_cat = document.createElement('p');
 const enter_cat = document.createElement('p');
@@ -13,8 +15,8 @@ const clothing_cat = document.createElement('p');
 
 let counter = 0; //determintes weeks
 
-let balance = 500;//starting budget/balance to be set at the start of the web app, also sets the budget
-
+let budget = 0;
+let balance = budget;//starting budget/balance to be set at the start of the web app, also sets the budget
 
 
 function balance_func() { //keeps balance from having more than 2 digits after floating point
@@ -30,32 +32,38 @@ class budgetPlanner{
         this.expenses1 = [ //main array containing the expenses 
             new Expense('Sandwich', 5.99, 'food'),
             new Expense('Movie', 11.99, 'entertainment'),
-            new Expense('Car monthly payment', 49.56, 'bills'),
+            new Expense('Car payment', 49.56, 'bills'),
             new Expense('Jeans', 45.49, 'clothing'),
             new Expense('Netflix', 12.99, 'entertainment'),
             new Expense('Electricity', 32.56, 'bills'),
             new Expense('Bikini', 30.43, 'clothing'),
             new Expense('Orange', 1.24, 'food'),
             new Expense('Sushi', 133.24, 'food'),
-            // balance = parseInt(prompt('budget?'))
         ],
 
         this.expense2 = [
             new Expense('Socks', 8.98, 'clothing'),
-            new Expense('PC', 8245, 'entertainment'),
+            new Expense('PC', 245, 'entertainment'),
             new Expense('Juice', 34.98, 'food'),
             new Expense('Gas', 45.98, 'bills'),
-            // balance = parseInt(prompt('budget?'))
         ],
 
         this.expense3 = [
             new Expense('underwear', 120, 'clothing'),
-            new Expense('Videogame', 825, 'entertainment'),
+            new Expense('Videogame', 85, 'entertainment'),
             new Expense('Donuts', 67.98, 'food'),
             new Expense('Gym', 66.98, 'bills'),
-            // balance = parseInt(prompt('budget?'))
+            
+        ],
+
+        this.expense4 = [
+            new Expense('Jacket', 21.48, 'clothing'),
+            new Expense('TV', 99, 'entertainment'),
+            new Expense('Soup', 12.98, 'food'),
+            new Expense('Electric Bill', 35.98, 'bills'),
             
         ]
+
 
         ];
     }
@@ -64,17 +72,34 @@ class budgetPlanner{
 
 changeWeeks(){ //toggles between the arrays to change weeks
     rightIcon.addEventListener( 'click', () => {
+
+        //checks if the week counter is higher than the quantity of weeks available and sets the counter to show the first week
+        if(counter >= this.weeks[counter].length - 1){ 
+            counter = -1;
+        }
+        
         counter++;
         console.log(counter);
-        // balance= parseInt(prompt('budget?'));
+        week_paragraph.innerText = `Week ${counter + 1}`;
         balance_func();
         this.display();
+
+        
     });
 
     leftIcon.addEventListener( 'click', () => {
+
+        //checks if the week counter is lower than the quantity of weeks available and resets the counter to the target the last week
+        if(counter <= 0){
+            counter = this.weeks.length;
+        }
+        
         counter--;
+        week_paragraph.innerText = `Week ${counter + 1}`;
         console.log(counter);
         this.display();
+
+
 
     });
 
@@ -121,19 +146,19 @@ changeWeeks(){ //toggles between the arrays to change weeks
 
         //appends and update the content of each category
         food_cat.className = 'cat';
-        food_cat.innerHTML = `Food: $${food_spent.toFixed(2)}`;
+        food_cat.innerHTML = `<span><i class=" food_icon cat_icon fas fa-utensils"></i>Food: </span><span>$${food_spent.toFixed(2)}</span>`;
         cat_div.appendChild(food_cat);
 
         enter_cat.className = 'cat';
-        enter_cat.innerHTML = `Entertainment: $${enter_spent.toFixed(2)}`;
+        enter_cat.innerHTML = `<span><i class="enter_icon cat_icon fas fa-tv"></i>Entertainment: </span> <span>$${enter_spent.toFixed(2)}</span>`;
         cat_div.appendChild(enter_cat);
 
         bills_cat.className = 'cat';
-        bills_cat.innerHTML = `Bills: $${bills_spent.toFixed(2)}`;
+        bills_cat.innerHTML = `<span><i class="bills_icon cat_icon fas fa-gas-pump"></i>Bills:</span> <span>$${bills_spent.toFixed(2)}</span>`;
         cat_div.appendChild(bills_cat);
 
         clothing_cat.className = 'cat';
-        clothing_cat.innerHTML = `Clothing: $${clothing_spent.toFixed(2)}`;
+        clothing_cat.innerHTML = `<span><i class="clothing_icon cat_icon fas fa-tshirt"></i>Clothing: </span><span>$${clothing_spent.toFixed(2)}</span>`;
         cat_div.appendChild(clothing_cat);
 
 
@@ -141,16 +166,17 @@ changeWeeks(){ //toggles between the arrays to change weeks
     }
     
     display(){ //displays expenses
-        balance_div.innerHTML= '';
+        budget_div.innerHTML= '';
         expenses_div.innerHTML = '';
         
+        budget = parseFloat(budget);
+        balanceHeader.classList.add('budget_num')
+        balanceHeader.innerHTML = `$${budget.toFixed(2)}`;
 
-        const balanceHeader = document.createElement('h1');
-        balanceHeader.innerHTML = `$${balance_func()}`;
-
-        balance_div.appendChild(balanceHeader);
+        budget_div.appendChild(balanceHeader);
         
-        main_div.appendChild(balance_div);
+        
+       
 
     
         let expenses = 0;
@@ -168,11 +194,28 @@ changeWeeks(){ //toggles between the arrays to change weeks
             <p class = 'expense_element inline'>$${item.price}</p> 
             `;
 
+            if(item.category === 'food'){
+                expense.classList.add('food_color');
+            }
+
+            if(item.category === 'entertainment'){
+                expense.classList.add('enter_color');
+            }
+
+            if(item.category === 'bills'){
+                expense.classList.add('bills_color');
+            }
+
+            if(item.category === 'clothing'){
+                expense.classList.add('clothing_color');
+            }
+
             const deleteIcon = document.createElement('i');
             
-            deleteIcon.className = ('expense_element deleteIcon inline far fa-trash-alt');
-
-            
+            deleteIcon.className = ('expense_element inline far fa-trash-alt deleteIcon');
+            // <i class="far fa-trash-alt"></i>
+            // <i class="fas fa-backspace"></i>
+            // <i class="fas fa-ban"></i>
             expenses_div.appendChild(expense);
 
             expense.appendChild(deleteIcon);
@@ -192,7 +235,10 @@ changeWeeks(){ //toggles between the arrays to change weeks
     displayBudget = (remainingBudget) => { //displays fixed budget
         const budget_p = document.querySelector('.budget');
         
-        budget_p.innerText = `$${remainingBudget}`;
+        budget_p.innerText = `$${remainingBudget.toFixed(2)}`;
+
+
+        
         
         
         }
@@ -203,6 +249,16 @@ changeWeeks(){ //toggles between the arrays to change weeks
         balance += this.weeks[counter][index].price;
         
         this.weeks[counter].splice(index,1);
+    }
+
+    updateBudget(){
+        budget_div.addEventListener('blur', () => {
+            budget = balanceHeader.innerText;
+            budget = budget.replace('$', '');
+            this.displayBudget(parseFloat(budget, 10));
+            
+            
+        });
     }
 
     
@@ -228,3 +284,4 @@ const planner = new budgetPlanner();
 planner.display();
 planner.displayBudget(balance);
 planner.changeWeeks();
+planner.updateBudget();
