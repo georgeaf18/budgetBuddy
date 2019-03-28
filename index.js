eb11aecfa655d37bfc19f49f8f81d188ac8b1f1a
JCSweet <sweetjor@gmail.com>
2019-03-27 21:14:57 -0400

    form validation added

--- index.js
+++ index.js
@@ -28,124 +28,124 @@ function balance_func() { //keeps balance from having more than 2 digits after f
 
 
 console.log("TCL: budgetPlanner -> constructor -> this.expenses1", typeof this.expenses1)
-class budgetPlanner{
-    constructor(){
+class budgetPlanner {
+    constructor() {
 
         this.weeks = [
-            
+
             this.expenses1 = [ //array containing the expenses 
-            new Expense('Sandwich', 5.99, 'food'),
-            new Expense('Movie', 11.99, 'entertainment'),
-            new Expense('Car payment', 49.56, 'bills'),
-            new Expense('Jeans', 45.49, 'clothing'),
-            new Expense('Netflix', 12.99, 'entertainment'),
-            new Expense('Electricity', 32.56, 'bills'),
-            new Expense('Bikini', 30.43, 'clothing'),
-            new Expense('Orange', 1.24, 'food'),
-            new Expense('Sushi', 133.24, 'food'),
-        ],
+                new Expense('Sandwich', 5.99, 'food'),
+                new Expense('Movie', 11.99, 'entertainment'),
+                new Expense('Car payment', 49.56, 'bills'),
+                new Expense('Jeans', 45.49, 'clothing'),
+                new Expense('Netflix', 12.99, 'entertainment'),
+                new Expense('Electricity', 32.56, 'bills'),
+                new Expense('Bikini', 30.43, 'clothing'),
+                new Expense('Orange', 1.24, 'food'),
+                new Expense('Sushi', 133.24, 'food'),
+            ],
 
-        this.expense2 = [ //arrays containing the expenses
-            new Expense('Socks', 8.98, 'clothing'),
-            new Expense('PC', 245, 'entertainment'),
-            new Expense('Juice', 34.98, 'food'),
-            new Expense('Gas', 45.98, 'bills'),
-        ],
+            this.expense2 = [ //arrays containing the expenses
+                new Expense('Socks', 8.98, 'clothing'),
+                new Expense('PC', 245, 'entertainment'),
+                new Expense('Juice', 34.98, 'food'),
+                new Expense('Gas', 45.98, 'bills'),
+            ],
 
-        this.expense3 = [ //arrays containing the expenses
-            new Expense('Sneakers', 120, 'clothing'),
-            new Expense('Videogame', 85, 'entertainment'),
-            new Expense('Donuts', 67.98, 'food'),
-            new Expense('Gym', 66.98, 'bills'),
-            
-        ],
+            this.expense3 = [ //arrays containing the expenses
+                new Expense('Sneakers', 120, 'clothing'),
+                new Expense('Videogame', 85, 'entertainment'),
+                new Expense('Donuts', 67.98, 'food'),
+                new Expense('Gym', 66.98, 'bills'),
 
-        this.expense4 = [ //arrays containing the expenses
-            new Expense('Jacket', 21.48, 'clothing'),
-            new Expense('TV', 99, 'entertainment'),
-            new Expense('Soup', 12.98, 'food'),
-            new Expense('Electric Bill', 35.98, 'bills'),
-            
-        ]
+            ],
+
+            this.expense4 = [ //arrays containing the expenses
+                new Expense('Jacket', 21.48, 'clothing'),
+                new Expense('TV', 99, 'entertainment'),
+                new Expense('Soup', 12.98, 'food'),
+                new Expense('Electric Bill', 35.98, 'bills'),
+
+            ]
 
 
         ];
     }
 
-/*************************************** Display functions start *****************************************/
+    /*************************************** Display functions start *****************************************/
 
-changeWeeks(){ //toggles between the arrays to change weeks
-    rightIcon.addEventListener( 'click', () => {
+    changeWeeks() { //toggles between the arrays to change weeks
+        rightIcon.addEventListener('click', () => {
 
-        //checks if the week counter is higher than the quantity of weeks available and sets the counter to show the first week
-        if(counter >= this.weeks.length - 1){ 
-            counter = -1;
-        }
-        
-        counter++;
-        week_paragraph.innerText = `Week ${counter + 1}`;
-        balance_func();
-        this.display();
+            //checks if the week counter is higher than the quantity of weeks available and sets the counter to show the first week
+            if (counter >= this.weeks.length - 1) {
+                counter = -1;
+            }
 
-        
-    });
-
-    leftIcon.addEventListener( 'click', () => {
-
-        //checks if the week counter is lower than the quantity of weeks available and resets the counter to the target the last week
-        if(counter <= 0){
-            counter = this.weeks.length;
-        }
-        
-        counter--;
-        week_paragraph.innerText = `Week ${counter + 1}`;
-        console.log(counter);
-        this.display();
+            counter++;
+            week_paragraph.innerText = `Week ${counter + 1}`;
+            balance_func();
+            this.display();
 
 
+        });
 
-    });
+        leftIcon.addEventListener('click', () => {
 
-}
+            //checks if the week counter is lower than the quantity of weeks available and resets the counter to the target the last week
+            if (counter <= 0) {
+                counter = this.weeks.length;
+            }
 
-    display_categories(){ //displays money spent in each category
-        food_cat.innerHTML = '';        
-        enter_cat.innerHTML = '';        
-        bills_cat.innerHTML = '';        
-        clothing_cat.innerHTML = '';        
-        
+            counter--;
+            week_paragraph.innerText = `Week ${counter + 1}`;
+            console.log(counter);
+            this.display();
+
+
+
+        });
+
+    }
+
+    display_categories() { //displays money spent in each category
+        food_cat.innerHTML = '';
+        enter_cat.innerHTML = '';
+        bills_cat.innerHTML = '';
+        clothing_cat.innerHTML = '';
+
         let food_spent = 0;
         let enter_spent = 0;
-        let bills_spent = 0 ;
+        let bills_spent = 0;
         let clothing_spent = 0;
 
 
         //fetches the prices from expenses and stores them in a variable for each category
-        const categories = this.weeks[counter].map( item => item.category);
-        const prices = this.weeks[counter].map( item => item.price);
+        const categories = this.weeks[counter].map(item => item.category);
+        const prices = this.weeks[counter].map(item => item.price);
         let i = 0;
 
-        for ( const element of categories){
-            
-            if(element === 'food'){
+        for (const element of categories) {
+
+            if (element === 'food') {
                 food_spent += parseFloat(prices[i]);
             }
 
-            if(element === 'entertainment'){
+            if (element === 'entertainment') {
                 enter_spent += parseFloat(prices[i]);
             }
 
-            if(element === 'bills'){
+            if (element === 'bills') {
                 bills_spent += parseFloat(prices[i]);
             }
 
-            if(element === 'clothing'){
+            if (element === 'clothing') {
                 clothing_spent += parseFloat(prices[i]);
             }
 
             i++;
         }
-        
+
 
         //appends and update the content of each category
         food_cat.className = 'cat';
@@ -165,58 +165,58 @@ changeWeeks(){ //toggles between the arrays to change weeks
         cat_div.appendChild(clothing_cat);
 
 
-        
+
     }
-    
-    display(){ //displays expenses
-        budget_div.innerHTML= '';
+
+    display() { //displays expenses
+        budget_div.innerHTML = '';
         expenses_div.innerHTML = '';
-        alert_div.innerHTML ='';
-        
+        alert_div.innerHTML = '';
+
         budget = parseFloat(budget);
         balanceHeader.classList.add('budget_num')
         balanceHeader.innerHTML = `$${budget.toFixed(2)}`;
 
         budget_div.appendChild(balanceHeader);
-        
-        
-       
 
-    
+
+
+
+
         let expenses = 0;
 
         //for loop that goes through every iteration to show each element in the main array
-         for(let i = 0; i < this.weeks[counter].length; i++){  
+        for (let i = 0; i < this.weeks[counter].length; i++) {
 
             const item = this.weeks[counter][i];
             expenses += item.price;
             const expense = document.createElement('div');
             expense.className = ('expense');
-        
+
 
             expense.innerHTML = `<p class = 'expense_element inline'>${item.name}</p> 
             <p class = 'expense_element inline'>$${item.price}</p> 
             `;
 
             //adds the colors to the expenses items  corresponding its categories
-            if(item.category === 'food' || item.category === 'Food'){
+            if (item.category === 'food' || item.category === 'Food') {
                 expense.classList.add('food_color');
             }
 
-            if(item.category === 'entertainment' || item.category === 'Entertainment'){
+            if (item.category === 'entertainment' || item.category === 'Entertainment') {
                 expense.classList.add('enter_color');
             }
 
-            if(item.category === 'bills' || item.category === 'Bills'){
+            if (item.category === 'bills' || item.category === 'Bills') {
                 expense.classList.add('bills_color');
             }
 
-            if(item.category === 'clothing' || item.category === 'Clothing'){
+            if (item.category === 'clothing' || item.category === 'Clothing') {
                 expense.classList.add('clothing_color');
             }
 
             const deleteIcon = document.createElement('i');
-            
+
             deleteIcon.className = ('expense_element inline far fa-trash-alt deleteIcon');
 
             expenses_div.appendChild(expense);
@@ -224,11 +224,11 @@ changeWeeks(){ //toggles between the arrays to change weeks
             expense.appendChild(deleteIcon);
 
 
-               
-            deleteIcon.addEventListener('click', () =>{
-            this.delete(i);
-            this.display();
-                });
+
+            deleteIcon.addEventListener('click', () => {
+                this.delete(i);
+                this.display();
+            });
         }
         balance = parseFloat(balanceHeader.innerText.replace('$', ""));
         this.displayBudget(balance - expenses);
@@ -236,103 +236,106 @@ changeWeeks(){ //toggles between the arrays to change weeks
         total_expenses.innerHTML = `Total $${expenses.toFixed(2)}`;
         total_cats.innerHTML = `Total $${expenses.toFixed(2)}`;
 
-        
-        
-        
+
+
+
 
         this.display_categories();
     }
 
     displayBudget = (remainingBudget) => { //displays fixed budget
         const budget_p = document.querySelector('.budget');
-        
+
         budget_p.innerText = `$${parseFloat(remainingBudget.toFixed(2))}`;
 
 
-        
+
 
         balance = parseFloat(remainingBudget.toFixed(2));
 
         //changes the color of the balance to red and displays alert when balance is below or equal to 0
-        if(remainingBudget <= 0){
+        if (remainingBudget <= 0) {
             alert_div.innerHTML = `Alert! You are out of money`;
             budget_p.style.color = 'red';
             alert_div.style.visibility = 'visible';
             alert_div.style.position = 'static';
-            
-        } if(remainingBudget >=.01){
+
+        } if (remainingBudget >= .01) {
             budget_p.style.color = '';
             alert_div.style.visibility = 'hidden';
             alert_div.style.position = 'absolute';
 
         }
 
-        
-        
-        
-        
-        
+
+
+
+
+
     }
 
-    delete(index){  //removes object from array when called and adds back balance
-        
-        if(typeof balance === 'number'){
-        balance = balance.toString();
-        } 
+    delete(index) {  //removes object from array when called and adds back balance
 
-        if (balance.includes('$')){
-        balance = parseFloat(balance.replace('$', ''));
-        } 
+        if (typeof balance === 'number') {
+            balance = balance.toString();
+        }
 
-        if (typeof balance === 'string'){
+        if (balance.includes('$')) {
+            balance = parseFloat(balance.replace('$', ''));
+        }
+
+        if (typeof balance === 'string') {
             balance = parseFloat(balance);
         }
 
         balance += this.weeks[counter][index].price;
 
-        
-        this.weeks[counter].splice(index,1);
+
+        this.weeks[counter].splice(index, 1);
     }
 
-    updateBudget(){
+    updateBudget() {
         budget_div.addEventListener('blur', () => {
             budget = balanceHeader.innerText;
             budget = budget.replace('$', '');
             this.displayBudget(parseFloat(budget, 10));
             balance = balanceHeader.innerText;
-            
-            
-            
+
+
+
         });
     }
-/*************************************** Display functions end *****************************************/
+    /*************************************** Display functions end *****************************************/
 
 
 
-/****************************************Pop-Up Function Begin*****************************************/
+    /****************************************Pop-Up Function Begin*****************************************/
 
-expenseForm() {
-    const submitButton = document.querySelector('#submitButton');
 
-// ********adds new form entry to the Expense array and hides the "new entry" popup on submit*********
-      
-submitButton.addEventListener('click', () => {
-    let item1 = document.querySelector('#itemInput').value;
-    let price1 = Number(document.querySelector('#priceInput').value);
-    let cat1 = document.querySelector('#exampleFormControlSelect1').value;
-    let newExpense = new Expense(item1, price1, cat1);
-    this.weeks[counter].push(newExpense);
-     // clear form after inputs
-    document.getElementById('expenseForm').reset();
-    document.getElementById("hidden").style.display = "none";
-    this.display();
-    });  
+    expenseForm() {
+        const submitButton = document.querySelector('#submitButton');
+
+        // ********adds new form entry to the Expense array and hides the "new entry" popup on submit*********
+
+        submitButton.addEventListener('click', () => {
+            let item1 = document.querySelector('#itemInput').value;
+            let price1 = Number(document.querySelector('#priceInput').value).toFixed(2);
+            let cat1 = document.querySelector('#exampleFormControlSelect1').value;
+            if (item1 && cat1 && price1) {
+                let newExpense = new Expense(item1, price1, cat1);
+                this.weeks[counter].push(newExpense);
+                // clear form after inputs
+                document.getElementById('expenseForm').reset();
+                document.getElementById("hidden").style.display = "none";
+                this.display();
+            }
+        });
+    }
 }
-}
-    const hidden = document.querySelector('#hidden');
-    const cancelButton = document.querySelector('#cancelButton');
-    const add_button = document.querySelector('.add_button');
-    const submitButton = document.querySelector('#submitButton');
+const hidden = document.querySelector('#hidden');
+const cancelButton = document.querySelector('#cancelButton');
+const add_button = document.querySelector('.add_button');
+const submitButton = document.querySelector('#submitButton');
 
 // *********makes new entry form visible when Add button is clicked**********
 add_button.addEventListener('click', () => {
@@ -345,18 +348,18 @@ add_button.addEventListener('click', () => {
 cancelButton.addEventListener('click', () => {
     document.getElementById("hidden").style.display = "none";
 });
-    
+
 /************************************Pop-Up Functions End**********************************************/
 
 
 
 
-class Expense{
-    constructor(name, price, category){
+class Expense {
+    constructor(name, price, category) {
         this.name = name;
         this.price = price;
         this.category = category;
-    
+
     }
 
 }
